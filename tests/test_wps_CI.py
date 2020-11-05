b4be4274f5b6ce4c9687b8031a683906e7cf29e4
Sangwon Lim <sangwonl@uvic.ca>
2020-11-05 15:55:46 -0800

    mark online

--- tests/test_wps_CI.py
+++ tests/test_wps_CI.py
@@ -6,6 +6,7 @@ from wps_tools.testing import run_wps_process
 from chickadee.processes.wps_CI import CI
 
 
+@pytest.mark.online
 @pytest.mark.parametrize(
     ("gcm_file", "obs_file", "varname", "out_file"),
     [
