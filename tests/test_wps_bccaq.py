e09758a8af9866db22502b659ec4da12c8f17670
cairosanders <cairosanders@hotmail.com>
2020-11-09 10:11:04 -0800

    Rewrite test_wps_bccaq for complex inputs

--- tests/test_wps_bccaq.py
+++ tests/test_wps_bccaq.py
@@ -11,8 +11,8 @@ from chickadee.processes.wps_bccaq import BCCAQ
     ("gcm_file", "obs_file", "var", "end_date", "out_file"),
     [
         (
-            resource_filename("tests", "data/tiny_gcm.nc"),
-            resource_filename("tests", "data/tiny_obs.nc"),
+            f"file://{resource_filename(__name__, 'data/tiny_gcm.nc')}",
+            f"file://{resource_filename(__name__, 'data/tiny_obs.nc')}",
             "tasmax",
             "1972-12-31",
             NamedTemporaryFile(suffix=".nc", prefix="output_", dir="/tmp", delete=True),
@@ -20,12 +20,14 @@ from chickadee.processes.wps_bccaq import BCCAQ
     ],
 )
 def test_wps_bccaq(gcm_file, obs_file, var, end_date, out_file):
-    datainputs = (
-        f"gcm_file={gcm_file};"
-        f"obs_file={obs_file};"
-        f"var={var};"
-        f"end_date={end_date};"
-        f"out_file={out_file.name};"
-    )
-    run_wps_process(BCCAQ(), datainputs)
-    out_file.close()
+    with NamedTemporaryFile(
+        suffix=".nc", prefix="output_", dir="/tmp", delete=True
+    ) as out_file:
+        datainputs = (
+            f"gcm_file=@xlink:href={gcm_file};"
+            f"obs_file=@xlink:href={obs_file};"
+            f"var={var};"
+            f"end_date={end_date};"
+            f"out_file={out_file.name};"
+        )
+        run_wps_process(BCCAQ(), datainputs)
