5fe6a9e3994623642925294c30293b25143c3601
Sangwon Lim <sangwonl@uvic.ca>
2020-11-09 13:21:52 -0800

    target base._libPaths()[0]

--- chickadee/utils.py
+++ chickadee/utils.py
@@ -19,9 +19,10 @@ def get_ClimDown(package, version):
     # Install and import R packages
     if not isinstalled("ClimDown"):
         utils = importr("utils")
-        utils.chooseCRANmirror(ind=1)
+        base = importr("base")
         utils.install_packages(
-            f"https://cloud.r-project.org/src/contrib/{package}_{version}.tar.gz"
+            f"https://cloud.r-project.org/src/contrib/{package}_{version}.tar.gz",
+            lib=base._libPaths()[0],
         )
 
     try:
