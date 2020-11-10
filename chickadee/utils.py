6ed73993429a078780c93a8477ddd68239c30e09
cairosanders <cairosanders@hotmail.com>
2020-11-09 16:46:46 -0800

    Formatting

--- chickadee/utils.py
+++ chickadee/utils.py
@@ -18,7 +18,10 @@ def install_R_package(package, version):
     if not isinstalled(package):
         utils = importr("utils")
         utils.chooseCRANmirror(ind=1)
-        utils.install_packages(f"https://cloud.r-project.org/src/contrib/{package}_{version}.tar.gz",)
+        utils.install_packages(
+            f"https://cloud.r-project.org/src/contrib/{package}_{version}.tar.gz",
+        )
+
 
 def get_R_package(package, version):
     try:
@@ -31,7 +34,7 @@ def get_doParallel():
     required_packages = [
         ("iterators", "1.0.13"),
         ("foreach", "1.5.1"),
-        ("doParallel", "1.0.16")
+        ("doParallel", "1.0.16"),
     ]
 
     for package, version in required_packages:
@@ -39,12 +42,14 @@ def get_doParallel():
 
     return get_R_package("doParallel", "1.0.16")
 
+
 def get_climdown():
     package = "ClimDown"
     version = "1.0.7"
-    
+
     install_R_package(package, version)
     return get_R_package(package, version)
+
 
 def set_r_options():
     robjects.r(
