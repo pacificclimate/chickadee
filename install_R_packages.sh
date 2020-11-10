7165a9deae8d829cf28a1cb2187b326007edbfa6
Sangwon Lim <sangwonl@uvic.ca>
2020-11-10 12:50:55 -0800

    install dependencies for ClimDown

--- install_R_packages.sh
+++ install_R_packages.sh
@@ -2,6 +2,13 @@
 R -e "
 dir.create(Sys.getenv('R_LIBS_USER'), recursive = TRUE);
 .libPaths(Sys.getenv('R_LIBS_USER'));
-install.packages('https://cloud.r-project.org/src/contrib/ClimDown_1.0.2.tar.gz');
+install.packages('PCICt');
+install.packages('udunits2');
+install.packages('ncdf4');
+install.packages('fields');
+install.packages('foreach');
+install.packages('seas');
+install.packages('abind');
+install.packages('https://cloud.r-project.org/src/contrib/ClimDown_1.0.2.tar.gz', dependencies=c('PCICt', 'udunits2', 'ncdf4', 'fields', 'foreach', 'seas', 'abind'))
 "
 
