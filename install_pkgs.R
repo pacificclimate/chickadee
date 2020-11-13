f534a936397957a9ef020300b0d59ca09b3f80c1
Sangwon Lim <sangwonl@uvic.ca>
2020-11-13 12:50:16 -0800

    add comments

--- install_pkgs.R
+++ install_pkgs.R
@@ -1,11 +1,15 @@
+# Create user library and install devtools package
 dir.create(Sys.getenv('R_LIBS_USER'), recursive = TRUE);
 .libPaths(Sys.getenv('R_LIBS_USER'));
+
+# Install devtools and its dependencies
 install.packages('usethis');
 install.packages('covr');
 install.packages('httr');
 install.packages('rversions');
 install.packages('devtools');
 
+# Install packages from requirements list
 requirements_file <- file("r_requirements.txt",open="r")
 data <-readLines(requirements_file)
 for (i in 1:length(data)){
@@ -17,6 +21,5 @@ for (i in 1:length(data)){
     } else {
         devtools::install_version(pkg, version = ver);
     }
-
 }
 close(requirements_file)
