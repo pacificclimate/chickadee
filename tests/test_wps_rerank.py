34634313caa5c587c94df0f9aed3ad6a6c4701d0
cairosanders <cairosanders@hotmail.com>
2020-11-20 13:03:11 -0800

    Remove num_cores parameter from test

--- tests/test_wps_rerank.py
+++ tests/test_wps_rerank.py
@@ -7,18 +7,17 @@ from chickadee.processes.wps_rerank import Rerank
 
 
 @pytest.mark.parametrize(
-    ("obs_file", "var", "num_cores", "qdm_file", "analogues_object"),
+    ("obs_file", "var", "qdm_file", "analogues_object"),
     [
         (
             f"file://{resource_filename(__name__, 'data/tiny_obs.nc')}",
             "tasmax",
-            4,
             f"file://{resource_filename(__name__, 'data/QDM_expected_output.nc')}",
             resource_filename(__name__, "data/analogues.rds"),
         ),
     ],
 )
-def test_wps_rerank(obs_file, var, num_cores, qdm_file, analogues_object):
+def test_wps_rerank(obs_file, var, qdm_file, analogues_object):
     with NamedTemporaryFile(
         suffix=".nc", prefix="output_", dir="/tmp", delete=True
     ) as out_file:
@@ -26,7 +25,6 @@ def test_wps_rerank(obs_file, var, num_cores, qdm_file, analogues_object):
             f"obs_file=@xlink:href={obs_file};"
             f"varname={var};"
             f"out_file={out_file.name};"
-            f"num_cores={num_cores};"
             f"qdm_file=@xlink:href={qdm_file};"
             f"analogues_object={analogues_object};"
         )
