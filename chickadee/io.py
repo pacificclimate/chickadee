727af018c7155a84798b33adc423076e2bd0be26
Eric Yvorchuk <eric.yvorchuk@gmail.com>
2024-04-15 12:37:51 -0700

    Rename tasmean to tg for unit options

--- chickadee/io.py
+++ chickadee/io.py
@@ -83,9 +83,9 @@ general_options_input = [
         data_type="string",
     ),
     LiteralInput(
-        "tasmean_units",
+        "tg_units",
         "Tasmean Units",
-        abstract="Units used for tasmean in output file",
+        abstract="Units used for tg (tasmean) in output file",
         max_occurs=1,
         default="celsius",
         data_type="string",
